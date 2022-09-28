ea2a2518b1d139b4fa68cba695f20a27a8449864
Whitney Catlin <whitneycatlin@whitneys-air.myfiosgateway.com>
2022-09-28 02:05:03 -0400

    changes

---
+++ Mapping_GeoJSON_Points/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicHJvdG9yeWFuNTcwIiwiYSI6ImNsOGw2Ymt2dzBiejYzeHBqOGU5d3UxZnoifQ.URL5PhO8m28goTtCUd5ynA";
--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 7);
 
 // Get data from cities.js
 let cityData = cities;
